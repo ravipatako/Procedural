cdc1d92504ff959a5f2c46680143965b67eecfcf
Ravi Pathak <35974311+ravipathak527@users.noreply.github.com>
2019-05-16 22:21:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Production Line Tracker\n";
+  std::cout << "Hello World!\n";
   std::string employAccount;
   std::cout << "Enter Account name ";
   getline (std::cin, employAccount);
   std::cout << "My account name is " << employAccount << ".\n";
   
+  std::cout << "Production Line Tracker\n";
   std::cout << "1. Add Employee Account.\n";
   std::cout << "2. Add Music Player \n";
   std::cout << "3. Add Movie Player \n";
