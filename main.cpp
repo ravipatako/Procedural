15170fe4a0d0f6f4271f5aa86762892c7a5bbdee
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-05-24 11:12:22 -0400

    In-Class
    Started working on it

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 //Procedural Project created by Ravi Pathak
 //Done during the course of COP 2001
 #include <iostream>
+using namespace std;
 
 int main() {
   //Bellow is commented out but prints out Hello World
   //then takes a string into Employ account and prints out the statement 
   std::cout << "Hello World!\n";
-  std::string employAccount;
-  std::cout << "Enter Account name ";
-  getline (std::cin, employAccount);
-  std::cout << "My account name is " << employAccount << ".\n";
-  
-  
+
   //Prints out the statements
-  std::cout << "Production Line Tracker\n";
-  std::cout <<"\n";
-  std::cout << "1. Add Employee Account\n";
-  std::cout << "2. Add Music Player\n";
-  std::cout << "3. Add Movie Player\n";
-  std::cout << "4. Display Production Statistics\n";
-  std::cout << "5. Exit\n";
+  cout << "Production Line Tracker\n" << endl;
+  cout << "1. Add Employee Account\n";
+  cout << "2. Add Music Player\n";
+  cout << "3. Add Movie Player\n";
+  cout << "4. Display Production Statistics\n";
+  cout << "5. Exit\n";
   
   
   
