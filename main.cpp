6d76caf954f8c67c0fb648813b6b25612b57cc91
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-05-25 20:29:31 -0400

    Fixing
    Currently working

--- main.cpp
+++ main.cpp
@@ -1,23 +1,50 @@
+/** @file main.cpp
+ *  @brief Brief description of file.
+ * //Procedural Project created by Ravi Pathak
+ * //Done during the course of COP 2001.
+ *
+ *  @author Ravi Pathak
+ *  @bug No known bugs.
+ */
 
-//Procedural Project created by Ravi Pathak
-//Done during the course of COP 2001
 #include <iostream>
+
 using namespace std;
 
-int main() {
-  //Bellow is commented out but prints out Hello World
-  //then takes a string into Employ account and prints out the statement 
-  std::cout << "Hello World!\n";
+/** @brief Short description of function.
+ *
+ *  Longer description of function.
+ *
+ *  @param One for each parameter with the name and a description
+ *  @return The word void or a description of what is returned
+ */
+void createEmployeeAccount();
 
-  //Prints out the statements
-  cout << "Production Line Tracker\n" << endl;
-  cout << "1. Add Employee Account\n";
-  cout << "2. Add Music Player\n";
-  cout << "3. Add Movie Player\n";
-  cout << "4. Display Production Statistics\n";
-  cout << "5. Exit\n";
-  
-  
-  
-  return 0;
+int main() {
+    
+    //Prints out the statements
+    cout << "Production Line Tracker\n" << endl;
+    cout << "1. Add Employee Account\n";
+    cout << "2. Add Music Player\n";
+    cout << "3. Add Movie Player\n";
+    cout << "4. Display Production Statistics\n";
+    cout << "5. Exit\n";
+
+    int selection;
+    cout << "Please make your selection" << endl;
+    cin >> selection;
+
+
+    switch (selection) {
+        case 1:
+            createEmployeeAccount();
+            break;
+
+    }
+
+
+}
+
+void createEmployeeAccount() {
+    cout << "You have are trying to add an Employee Account " << endl;
 }
