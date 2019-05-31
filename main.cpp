a7e574dfc9180b8e83241000a750b5164cca46b0
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-05-31 11:41:47 -0400

    Update and worked with Professor

--- main.cpp
+++ main.cpp
@@ -8,76 +8,9 @@
  */
 
 #include <iostream>
+#include "prototype.h"
 
 using namespace std;
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void createEmployeeAccount();
-
-/** @brief Adds a music player.
- *
- *  User can choose to add a new Music Player and cna give input on
- *  the type and format
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void addMusicPlayer();
-
-/** @brief Adds a movie Player.
- *
- *  Allows the user to create a movie player and gives them
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void addMoviePlayer();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void showProduceStats();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void closeProgram();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void invalidNumber();
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void printMenu();
 
 int main() {
     int selection;
