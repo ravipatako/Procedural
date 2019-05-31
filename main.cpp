8621cc18cd6908fc53b8299d1c740067c161e7ae
ravipathak527 <35974311+ravipathak527@users.noreply.github.com>
2019-05-31 11:43:59 -0400

    Working

--- main.cpp
+++ main.cpp
@@ -4,122 +4,52 @@
  * //Done during the course of COP 2001 in Summer of 2019.
  *
  *  @author Ravi Pathak
- *  @bug No known bugs currently.
+ *  @bug No known bugs currently .
  */
 
 #include <iostream>
+#include "prototype.h"
 
 using namespace std;
 
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void createEmployeeAccount();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void addMusicPlayer();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void addMoviePlayer();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void showProduceStats();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void closeProgram();
-
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void invalidNumber();
-/** @brief Creates the employee Account.
- *
- *  The functions takes info form the user and allows them
- *  to create a new Employee Account
- *
- *  @param none -- currently
- *  @return The word void or a description of what is returned
- */
-void menuLoop();
-
 int main() {
-
-    //Prints out the statements
-    cout << "Production Line Tracker\n" << endl;
-    cout << "1. Add Employee Account\n";
-    cout << "2. Add Music Player\n";
-    cout << "3. Add Movie Player\n";
-    cout << "4. Display Production Statistics\n";
-    cout << "5. Exit\n";
-
     int selection;
-    cout << "Please make your selection" << endl;
-    cin >> selection;
+    char repeat;
+    do {
+        printMenu();
 
-
-    switch (selection) {
-        case 1:
-            createEmployeeAccount();
-            break;
-        case 2:
-            addMusicPlayer();
-            break;
-        case 3:
-            addMoviePlayer();
-            break;
-        case 4:
-            showProduceStats();
-            break;
-        case 5:
-            closeProgram();
-            break;
-        default:
-            invalidNumber();
-    }
-
-    while (selection >= 6){
-        cout << "Sorry that option doesnt exist. " << endl;
-        cout << "Please reselect a number from the menu:" << endl;
+        cout << "Please make your selection" << endl;
         cin >> selection;
-    }
+
+
+        switch (selection) {
+            case 1:
+                createEmployeeAccount();
+                break;
+            case 2:
+                addMusicPlayer();
+                break;
+            case 3:
+                addMoviePlayer();
+                break;
+            case 4:
+                showProduceStats();
+                break;
+            case 5:
+                closeProgram();
+                break;
+            default:
+                invalidNumber();
+        }
+
+        while (selection >= 6 || selection <= 0) {
+            printMenu();
+            cout << "Please reselect a number from the menu:" << endl;
+            cin >> selection;
+        }
+        cout << "Would you like to go back to the menu? " << endl;
+        cin >> repeat;
+    } while (repeat == 'Y' ); // The do while loop was created from the CSO Chapter 5 slide 33
 }
 
 void createEmployeeAccount() {
@@ -158,13 +88,19 @@ void showProduceStats() {
 }
 
 void closeProgram() {
-    cout << "You have selected to add a new Music Player " << endl;
+    cout << "Good Bye! " << endl;
 }
 
 void invalidNumber() {
     cout << "Sorry that's not an option" << endl;
 }
 
-void menuLoop(){
-
+void printMenu(){
+    //Prints out the statements
+    cout << "Production Line Tracker\n" << endl;
+    cout << "1. Add Employee Account\n";
+    cout << "2. Add Music Player\n";
+    cout << "3. Add Movie Player\n";
+    cout << "4. Display Production Statistics\n";
+    cout << "5. Exit\n";
 }
---
+++ prototype.h
@@ -0,0 +1,75 @@
+//
+// Created by Ravi Pathak on 2019-05-31.
+//
+
+#ifndef HELLO_PROTOTYPE_H
+#define HELLO_PROTOTYPE_H
+/** @brief Creates the employee Account.
+ *
+ *  The functions takes info form the user and allows them
+ *  to create a new Employee Account
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void createEmployeeAccount();
+
+/** @brief Adds a music player.
+ *
+ *  User can choose to add a new Music Player and cna give input on
+ *  the type and format
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void addMusicPlayer();
+
+/** @brief Adds a movie Player.
+ *
+ *  Allows the user to create a movie player and gives them
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void addMoviePlayer();
+
+/** @brief Creates the employee Account.
+ *
+ *  The functions takes info form the user and allows them
+ *  to create a new Employee Account
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void showProduceStats();
+
+/** @brief Creates the employee Account.
+ *
+ *  The functions takes info form the user and allows them
+ *  to create a new Employee Account
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void closeProgram();
+
+/** @brief Creates the employee Account.
+ *
+ *  The functions takes info form the user and allows them
+ *  to create a new Employee Account
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void invalidNumber();
+/** @brief Creates the employee Account.
+ *
+ *  The functions takes info form the user and allows them
+ *  to create a new Employee Account
+ *
+ *  @param none -- currently
+ *  @return The word void or a description of what is returned
+ */
+void printMenu();
+
+#endif //HELLO_PROTOTYPE_H
